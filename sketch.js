e8c441d4230e40986b6d7bf33087987b001d1af9
rakesh <rakeshmirji@gmail.com>
2024-02-17 19:01:38 +0530

    pulse commit

--- sketch.js
+++ sketch.js
@@ -1,3 +1,36 @@
+const min = 63
+const max = 89
+var val = (max+min)/2
+var heartRate=0
+var pulse = document.getElementById("pulse")
+
+function beat(){
+  function getRandomInt(max) {
+      return Math.round(Math.random() * max);
+  }
+  let elem = getRandomInt(1) ? 1 : -1
+  if(val+elem >= min && val+elem <= max){
+      val+=elem
+  }
+  if (val >= heartRate+5 || val<=heartRate-4 ) {
+    heartRate=val
+  }
+  // console.log(val,heartRate);
+  return heartRate
+}
+function calulateBP(){
+  if( myFaces.length>0){
+    pulse.innerText = beat();
+
+  }else{
+    pulse.innerText = 0;
+    heartRate = 0;
+    val = (max+min)/2
+  }
+}
+
+
+
 // A choice for number of keypoints: 7,33,68,468
 
 // === bare minimum 7 points ===
@@ -40,15 +73,22 @@ var capture; // webcam capture, managed by p5.js
 
 // Load the MediaPipe facemesh model assets.
 facemesh.load().then(function(_model){
+  _model.pipeline.maxFaces=1
   console.log("model initialized.")
   statusText = "Model loaded."
   facemeshModel = _model;
+  console.log(_model);
 })
 
 
 function setup() {
-  createCanvas(640, 480);
-  capture = createCapture(VIDEO);
+  createCanvas(1280, 960);
+
+  capture = createCapture(VIDEO); 
+  capture.elt.width = 1280;
+  capture.elt.height = 960;
+  console.log(capture.elt.style);
+
   // this is to make sure the capture is loaded before asking facemesh to take a look
   // otherwise facemesh will be very unhappy
   capture.elt.onloadeddata = function(){
@@ -62,7 +102,6 @@ function setup() {
 
 // draw a face object returned by facemesh
 function drawFaces(faces,filled){
-
   for (var i = 0; i < faces.length; i++){
     const keypoints = faces[i].scaledMesh;
 
@@ -79,7 +118,6 @@ function drawFaces(faces,filled){
       var a = keypoints[TRI[j  ]];
       var b = keypoints[TRI[j+1]];
       var c = keypoints[TRI[j+2]];
-      
       if (filled){
         var d = [(a[0]+b[0]+c[0])/6, (a[1]+b[1]+c[1])/6];
         var color = get(...d);
@@ -101,7 +139,8 @@ function packFace(face,set){
   var ret = {
     scaledMesh:[],
   }
-  console.log(face);
+  // console.log("Upper Lips : ",face.annotations.lipsUpperInner,face.annotations.lipsUpperOuter);
+  // console.log("Lower Lips : ",face.annotations.lipsLowerInner,face.annotations.lipsLowerOuter);
   for (var i = 0; i < set.length; i++){
     var j = set[i];
     ret.scaledMesh[i] = [
@@ -133,33 +172,23 @@ function draw() {
         statusText = "Confidence: "+ (Math.round(_faces[0].faceInViewConfidence*1000)/1000);
         
       }
-      
     })
   }
-  
+
+  calulateBP()
   background(200);
   
   // first draw the debug video and annotations
   push();
   // scale(0.5); // downscale the webcam capture before drawing, so it doesn't take up too much screen sapce
+  // console.log(capture.elt.width, capture.height)
   image(capture, 0, 0, capture.width, capture.height);
+  console.log(capture.width,capture.height);
   noFill();
+  scale(2)
   stroke(0,150, 255, 100);
   drawFaces(myFaces); // draw my face skeleton
   pop();
-  
-  
-  // now draw all the other users' faces (& drawings) from the server
-  // push()
-  
-  // scale(2);
-  // strokeWeight(3);
-  // noFill();
-  // drawFaces(myFaces);
-  // pop();
-  
-  // push();
-  // fill(255,0,0);
-  // text(statusText,2,60);
-  // pop();
+
 }
+
